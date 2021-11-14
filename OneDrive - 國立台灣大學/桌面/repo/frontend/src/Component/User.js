69e92fe54730ce55541cf8a9388faf8f9f3a34e7
Alan Tsai <a89121248@ntu.im>
2021-11-14 13:54:09 +0800

    frontend

--- OneDrive - 國立台灣大學/桌面/repo/frontend/src/Component/User.js
+++ OneDrive - 國立台灣大學/桌面/repo/frontend/src/Component/User.js
@@ -19,10 +19,10 @@ const User = ({ setUserID, setPassword }) => {
         <div>
             <Space className='stopLoss'>
                 <h3>輸入帳號</h3>
-                <Input placeholder="Basic usage"
+                <Input placeholder="請輸入帳號"
                        onChange={onChangeID} />
                 <h3>輸入密碼</h3>
-                <Input placeholder="Basic usage"
+                <Input placeholder="請輸入密碼"
                        onChange={onChangePwd} />
             </Space>
         </div>
--- backend/STOCK/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from myapp import views
-
-urlpatterns = [
-    url('signup', views.signup),
-    url('stockinformation', views.stockinfromation),
-    url('backtest', views.backtest),
-    url('render', views.render)
-]
